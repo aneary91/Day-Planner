9d28bfa9454c072c280033fa6fa5b608b6155fc3
AllisonWonderland <aneary91@gmail.com>
2021-04-06 16:54:36 -0500

    making changes

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 $(document).ready(function () {
-  // set event listener fot save button
+  // set event listener for save button
   $(".saveBtn").on("click", function () {
     // targeting the descreiption and saving the input as a value
-    var textInput = $(this).siblings("description").val();
+    // var textInput = $(this).siblings("description").val();
+    var siblings = $(this).parent().children();
+    var userEvent = siblings[1].value;
+   
     // targeting the parent container throught the Id (hour-(num))
-    var timeSection = $(this).parent().attr("id");
-    // using scoed variable to save to local stoage in the broswer
-    localStorage.setItem(timeSection, textInput);
+    // var timeSection = $(this).parent().attr("id");
+    var hourInputId = this.parentNode.id;
+
+
+  
+    localStorage.setItem(hourInputId, userEvent);
+
+   
   });
 
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-  function colorHour() {
+  function hourColors() {
     var actualHour = moment().hours();
     // loop over the time block in html
     $(".time-block").each(function () {
-      var blockTime = parseInt($(this).attr("id").split("-")[1]);
+      var timeBlock = parseInt($(this).attr("id").split("-")[1]);
 
-      if (blockTime < actualHour) {
+      if (timeBlock < actualTime) {
         $(this).addClass("past");
-      } else if (blockTime === actualHour) {
+      } else if (timeBlock === actualTime) {
         $(this).removeClass("past");
         $(this).addClass("present");
       } else {
@@ -29,16 +37,21 @@ $(document).ready(function () {
       }
     });
   }
-  colorHour();
+
+  hourColors();
+
+
+
 });
 
 // load local storage
-$("#hour-9 .description").val(localStorage.getTime("hour-9"))
-$("#hour-10 .description").val(localStorage.getTime("hour-10"))
-$("#hour-11 .description").val(localStorage.getTime("hour-11"))
-$("#hour-12 .description").val(localStorage.getTime("hour-12"))
-$("#hour-1 .description").val(localStorage.getTime("hour-1"))
-$("#hour-2 .description").val(localStorage.getTime("hour-2"))
-$("#hour-3 .description").val(localStorage.getTime("hour-3"))
-$("#hour-4 .description").val(localStorage.getTime("hour-4"))
-$("#hour-5 .description").val(localStorage.getTime("hour-5"))
+$("#hour-9 .description").val(localStorage.getTime("hour-9"));
+$("#hour-10 .description").val(localStorage.getTime("hour-10"));
+$("#hour-11 .description").val(localStorage.getTime("hour-11"));
+$("#hour-12 .description").val(localStorage.getTime("hour-12"));
+$("#hour-13 .description").val(localStorage.getTime("hour-13"));
+$("#hour-14 .description").val(localStorage.getTime("hour-14"));
+$("#hour-15 .description").val(localStorage.getTime("hour-15"));
+$("#hour-16 .description").val(localStorage.getTime("hour-16"));
+$("#hour-17 .description").val(localStorage.getTime("hour-17"));
+
